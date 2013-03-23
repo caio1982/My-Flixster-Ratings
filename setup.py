ca3f235a0633edb2fda85e707768bf3160d8b0e1
Caio Begotti <caio1982@gmail.com>
2013-03-23 20:32:51 -0300

    minimal distutils configuration for distribution

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name = "mfr",
+    version = "0.0.1",
+    packages = ["mfr"],
+    license = "Public Domain",
+    author = "Caio Begotti",
+    author_email = "caio1982@gmail.com",
+    description = "Flixster.com ratings handling module",
+    url = "https://github.com/caio1982/My-Flixster-Ratings",
+    keywords = ["json", "csv", "movies", "rating", "flixster"]
+)
