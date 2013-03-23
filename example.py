2e532ef5269277fdb43e6cc6c55912910b558050
Caio Begotti <caio1982@gmail.com>
2013-03-23 17:58:23 -0300

    importing a simple example on how to use the module

---
+++ example.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Caio Begotti
+# <caio1982@gmail.com>
+# Public Domain
+
+from mfr import ratings
+
+r = ratings.Ratings("845548311", 1)
+backup = "myflixsterratings.json"
+
+if r.write(backup):
+    print "Ratings saved!"
+
+data = r.read(backup)
+print data
