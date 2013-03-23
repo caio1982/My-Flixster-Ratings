cbd5953ed615bc56f5a5e1edaed1494cd3139e01
Caio Begotti <caio1982@gmail.com>
2013-03-23 18:50:03 -0300

    a better explained example

--- example.py
+++ example.py
@@ -7,11 +7,18 @@
 
 from mfr import ratings
 
-r = ratings.Ratings("845548311", 3)
+# fetch ratings (first 100 by default)
+r = ratings.Ratings("845548311")
 backup = "myflixsterratings.json"
 
+# write the json to a file
 if r.write(backup):
     print "Ratings saved!"
 
+# read the json from the file
 data = r.read(backup)
-print data
+print "Ratings data length is", len(data)
+
+# export it to a real file
+if r.export("dump.csv"):
+    print "CSV file exported!"
